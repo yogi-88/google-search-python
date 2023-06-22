b8b3f681645bb899429cf956d328fca679d54e8b
YOGITA NEGI <116275181+yogi-88@users.noreply.github.com>
2023-06-22 22:18:17 +0100

    Update googlesearchisins_v1.py
    Here are some changes made to enhance the Pythonic style:
    
    Used GOOGLE_SEARCH_URL and HEADERS as constants in uppercase.
    Utilized with open statement to handle file operations, which automatically handles file closing.
    Utilized list comprehension to split lines from the file into isin_codes.
    Removed unnecessary file closing and opening statements.
    Used an inline if-else statement (search.find('a')['href'] if search.find('a') else "Not available in Google") to handle cases where the link is not available.
    Renamed variables to conform to snake_case naming convention.
    Removed unnecessary parentheses and semicolons.
    These changes make the code more concise, readable, and conformant to Pythonic style guidelines.

--- googlesearch/googlesearchisins_v1.py
+++ googlesearch/googlesearchisins_v1.py
@@ -4,34 +4,31 @@ from random import randint
 from time import sleep
 import requests
 
-url = 'https://www.google.com/search'
-headers = {
+GOOGLE_SEARCH_URL = 'https://www.google.com/search'
+HEADERS = {
 	'Accept' : '*/*',
 	'Accept-Language': 'en-US,en;q=0.5',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82',
 }
-googlesearch = []
+google_search_results = []
 
-file = open("./isins.txt")
-lines = file.read().splitlines()
+with open("isins.txt", "r") as file:
+	isin_codes = file.read().splitlines()
 file.close()
 
-for isin in lines:
+for isin in isin_codes:
 	parameters = {'q': isin}
 	print(parameters)
-	content = requests.get(url, headers=headers, params=parameters).text
+	content = requests.get(GOOGLE_SEARCH_URL, headers=HEADERS, params=parameters).text
 	sleep(randint(1, 5))
 	soup = BeautifulSoup(content, 'html.parser')
 	search = soup.find(id='search')
-	try:
-		first_link = search.find('a')['href']
-	except TypeError:
-		first_link = "Not available in Google"
+	first_link = search.find('a')['href'] if search.find('a') else "Not available in Google"
 
 	tempdata = {
 		'ISIN': isin,
 		'First Link': first_link
 	}
-	googlesearch.append(tempdata)
-df = pd.DataFrame.from_dict(googlesearch)
+	google_search_results.append(tempdata)
+df = pd.DataFrame(google_search_results)
 print(df)
